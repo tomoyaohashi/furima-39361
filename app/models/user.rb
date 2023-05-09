c79befc0e3597515f915cdc93b7e691948c4cddb
tomoyaohashi <t.ohashi1016@gmail.com>
2023-05-09 17:32:37 +0900

    単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :surname, presence: true
-         validates :name, presence: true
-         validates :surnamekana, presence: true
-         validates :namekana, presence: true
          validates :birthday, presence: true
 
-         with_options presence: true, format: { with: /[ぁ-んァ-ヶ一-龥々ー]/, message: '全角文字を使用してください' } do
-          validates :surname
-          validates :surname
+         with_options presence: true, format: { with: /[ぁ-んァ-ヶ一-龥々ー]/ } do
+          validates :surname, presence: true
+          validates :name, presence: true
         end
 
-        with_options presence: true, format: { with: /[ァ-ヶ]/, message: '全角カナ文字を使用してください' } do
-          validates :surnamekana
-          validates :namekana
+        with_options presence: true, format: { with: /[ァ-ヶ]/ } do
+          validates :surnamekana, presence: true
+          validates :namekana, presence: true
         end
 
         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze

2c562e2647c82a774d6e1f65947e29182b9f7904
tomoyaohashi <t.ohashi1016@gmail.com>
2023-05-15 21:32:47 +0900

    修正#3

--- app/models/history_buy.rb
+++ app/models/history_buy.rb
@@ -4,7 +4,6 @@ class HistoryBuy
 
   validates :postal_id, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :building, presence: true, allow_blank: true
   validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: 'should be 10 to 11 digits' }
 
   validates :postal_id, :prefecture_id, :token, :city, :address, :phone, :user_id, :item_id, presence: true
